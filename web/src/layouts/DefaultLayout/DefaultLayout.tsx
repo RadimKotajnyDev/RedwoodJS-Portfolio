b7ebf7977e40eb7cae35eddf68ab1fce56fd92f9
radimkotajnydev <radimkotajny@gmail.com>
2023-03-29 21:37:51 +0200

    vlna jak z fyziky

--- web/src/layouts/DefaultLayout/DefaultLayout.tsx
+++ web/src/layouts/DefaultLayout/DefaultLayout.tsx
@@ -1,21 +1,37 @@
 import Navbar from "src/components/Navbar"
-import { Box } from "@chakra-ui/react";
+import {Box, Image} from "@chakra-ui/react";
 
 type DefaultLayoutProps = {
   children?: React.ReactNode
 }
 
 const DefaultLayout = ({ children }: DefaultLayoutProps) => {
-  return <
-    //h="100vh"
+  return <Box
+    pos="relative"
+    h="100vh"
     //background="blackAlpha.900" //TODO: fix dark mode
     //color="white"
   >
-    <Navbar />
-    <main>
-      {children}
-    </main>
-  </>
+    <Box
+      pos="absolute"
+      zIndex="1"
+      w="full"
+    >
+      <Navbar />
+      <main>
+        {children}
+      </main>
+    </Box>
+    <Box
+      zIndex="-1"
+      boxSize="full"
+      pos="absolute"
+    >
+      <Image src="/waves.svg" alt="waves" w="full" h="full"
+             z-index="0" objectFit="cover"
+      />
+    </Box>
+  </Box>
 }
 
 export default DefaultLayout
--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,4 @@
 import { Box, Image, Flex, Spacer, chakra, shouldForwardProp } from '@chakra-ui/react'
-
 import { TypeAnimation } from 'react-type-animation'
 
 import { MetaTags } from '@redwoodjs/web'
@@ -19,12 +18,15 @@ const HomePage = () => {
   return (
     <>
       <MetaTags
-        title="Radim Kotajny Portfolio"
+        title="Radim Kotajny"
         description="Radim Kotajny Portfolio Website"
       />
       <Box>
         <Flex>
-          <Flex as="div" direction="column" ml={100}>
+          <Flex as="div" direction="column" ml={{
+            base: 10,
+            md: 100
+          }}>
             <TypeAnimation
               cursor={false}
               //omitDeletionAnimation={true}
@@ -42,67 +44,10 @@ const HomePage = () => {
             />
           </Flex>
           <Spacer />
-          {/*
-            <ChakraBox
-            initial={{ scale: 0, opacity: 0 }}
-            animate={{ scale: 1, opacity: 1 }}
-            transition={{ delay: 4, duration: 0.5 }}
-            boxSize="2xl" pos="absolute" ml="60vw">
-            <Image src="/RadimLogo.png" alt="Radim Kotajny Logo" />
-          </ChakraBox>
-          */}
         </Flex>
-        {/*
-        <ChakraBox
-          initial={{ scale: 0, opacity: 0 }}
-          animate={{ scale: 1, opacity: 1 }}
-          transition={{ delay: 3, duration: 0.5 }}
-          boxSize="3xl">
-          <Image src="/welcome.png" alt="welcome code" />
-        </ChakraBox>
-       */}
       </Box>
     </>
   )
 }
 
 export default HomePage
-
-/*
-<motion.div
-          initial={{ opacity: .75 }}
-          transition={{ duration: 2 }}
-          animate={{ opacity: 0 }}>
-          <chakra.h1
-            bgGradient="linear(to-l, #7928CA, #FF0080)"
-            bgClip="text"
-            fontSize="6xl"
-          >
-            &lt;h1&gt;
-          </chakra.h1>
-        </motion.div>
-        <chakra.h1
-          bgGradient="linear(to-l, #7928CA, #FF0080)"
-          bgClip="text"
-          fontSize="6xl"
-          fontWeight={900}
-        >
-          Radim Kotajny <br /> Frontend developer
-        </chakra.h1>
-        <motion.div
-          initial={{ opacity: 1 }}
-          transition={{ duration: 2 }}
-          animate={{ opacity: 0 }}>
-          <chakra.h1
-            bgGradient="linear(to-l, #7928CA, #FF0080)"
-            bgClip="text"
-            fontSize="6xl"
-            fontWeight="thin"
-          >
-            <br />&lt;/h1&gt;
-          </chakra.h1>
-        </motion.div>
-        <Box boxSize="lg">
-          <Image src="/RadimLogo.png" alt="Radim Kotajny Logo" />
-        </Box>
- */
